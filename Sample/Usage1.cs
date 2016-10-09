8d376c229c9c1913a9fa7c9c750ca73ad8d51d54
cactuaroid <solamiredoredore@gmail.com>
2016-10-09 13:54:03 -0700

    Organize sample unit test with categories of accessibility, static, virtual, type.

--- Sample/Usage1.cs
+++ Sample/Usage1.cs
@@ -11,27 +11,32 @@ namespace Sample
     {
         private class Base
         {
-            private string _private = "private member";
-            private static string _privateStatic = "private static member";
+            // accessibility
+            private string _private = "private";
             protected string Protected { get; private set; }
-            protected static string ProtectedStatic { get; private set; }
-            protected virtual string ProtectedVirtual { get; private set; }
+            internal string Internal { get; private set; }
+            protected internal string ProtectedInternal { get; private set; }
             public string Public { get; private set; }
-            public static string PublicStatic { get; private set; }
-            public virtual string PublicVirtual { get; private set; }
 
+            // static
+            private static string _privateStatic = "private static";
+
+            // virtual
+            protected virtual string ProtectedVirtual { get; private set; }
+
+            // type
             private int _struct = 0;
             private IEnumerable<int> _interface = new int[] { };
             private int[] _array = new int[] { };
+            private dynamic _dynamic = "dynamic member";
 
             public Base()
             {
-                Protected = "protected member";
-                ProtectedStatic = "protected static member";
-                ProtectedVirtual = "protected virtual member";
-                Public = "public member";
-                PublicStatic = "public static member";
-                PublicVirtual = "public virtual member";
+                Protected = "protected";
+                Internal = "internal";
+                ProtectedInternal = "protected internal";
+                Public = "public";
+                ProtectedVirtual = "protected virtual";
             }
         }
 
@@ -45,30 +50,30 @@ namespace Sample
             var derived = new Derived();
 
             derived.SetPrivate("_private", "changed");
-            derived.SetPrivate("_privateStatic", "changed");
             derived.SetPrivate("Protected", "changed");
-            derived.SetPrivate("ProtectedVirtual", "changed");
-            derived.SetPrivate("ProtectedStatic", "changed");
+            derived.SetPrivate("Internal", "changed");
+            derived.SetPrivate("ProtectedInternal", "changed");
             derived.SetPrivate("Public", "changed");
-            derived.SetPrivate("PublicStatic", "changed");
-            derived.SetPrivate("PublicVirtual", "changed");
+            Assert.AreEqual("changed", derived.GetPrivate("_private"));
+            Assert.AreEqual("changed", derived.GetPrivate("Protected"));
+            Assert.AreEqual("changed", derived.GetPrivate("Internal"));
+            Assert.AreEqual("changed", derived.GetPrivate("ProtectedInternal"));
+            Assert.AreEqual("changed", derived.GetPrivate("Public"));
+
+            derived.SetPrivate("_privateStatic", "changed");
+            Assert.AreEqual("changed", derived.GetPrivate("_privateStatic"));
+
+            derived.SetPrivate("ProtectedVirtual", "changed");
+            Assert.AreEqual("changed", derived.GetPrivate("ProtectedVirtual"));
 
             derived.SetPrivate("_struct", 123);
             derived.SetPrivate("_interface", new List<int>() { 1, 2, 3 });
             derived.SetPrivate("_array", new int[] { 1, 2, 3 });
-
-            Assert.AreEqual("changed", derived.GetPrivate("_private"));
-            Assert.AreEqual("changed", derived.GetPrivate("_privateStatic"));
-            Assert.AreEqual("changed", derived.GetPrivate("Protected"));
-            Assert.AreEqual("changed", derived.GetPrivate("ProtectedVirtual"));
-            Assert.AreEqual("changed", derived.GetPrivate("ProtectedStatic"));
-            Assert.AreEqual("changed", derived.GetPrivate("Public"));
-            Assert.AreEqual("changed", derived.GetPrivate("PublicStatic"));
-            Assert.AreEqual("changed", derived.GetPrivate("PublicVirtual"));
-
+            derived.SetPrivate("_dynamic", new List<int>() { 1, 2, 3 });
             Assert.AreEqual(123, derived.GetPrivate("_struct"));
             Assert.AreEqual(3, derived.GetPrivate<IEnumerable<int>>("_interface").Count());
             Assert.AreEqual(3, derived.GetPrivate<int[]>("_array").Length);
+            Assert.AreEqual(3, derived.GetPrivate<dynamic>("_dynamic").Count);
         }
     }
 }
