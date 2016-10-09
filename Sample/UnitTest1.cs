4efff98679e6dc227f23c1a0f0cb5211251e546c
cactuaroid <solamiredoredore@gmail.com>
2016-10-09 00:55:46 -0700

    Add sample unit tests.

--- Sample/UnitTest1.cs
+++ Sample/UnitTest1.cs
@@ -3,52 +3,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Sample
 {
-    public class Base
-    {
-        private string _private = "private member";
-        private static string _privateStatic = "private static member";
-        protected string Protected { get; private set; }
-        protected static string ProtectedStatic { get; private set; }
-        protected virtual string ProtectedVirtual { get; private set; }
-        public string Public { get; private set; }
-        public static string PublicStatic { get; private set; }
-        public virtual string PublicVirtual { get; private set; }
-
-        public Base()
-        {
-            Protected = "protected member";
-            ProtectedStatic = "protected static member";
-            ProtectedVirtual = "protected virtual member";
-            Public = "public member";
-            PublicStatic = "public static member";
-            PublicVirtual = "public virtual member";
-        }
-    }
-
-    public class Derived : Base
-    {
-    }
-
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void CanGetMembers()
+        private class Base
         {
-            var derived = new Derived();
+            private string _private = "private member";
+            private static string _privateStatic = "private static member";
+            protected string Protected { get; private set; }
+            protected static string ProtectedStatic { get; private set; }
+            protected virtual string ProtectedVirtual { get; private set; }
+            public string Public { get; private set; }
+            public static string PublicStatic { get; private set; }
+            public virtual string PublicVirtual { get; private set; }
 
-            Assert.IsNotNull(derived.GetPrivate("_private"));
-            Assert.IsNotNull(derived.GetPrivate("_privateStatic"));
-            Assert.IsNotNull(derived.GetPrivate("Protected"));
-            Assert.IsNotNull(derived.GetPrivate("ProtectedStatic"));
-            Assert.IsNotNull(derived.GetPrivate("ProtectedVirtual"));
-            Assert.IsNotNull(derived.GetPrivate("Public"));
-            Assert.IsNotNull(derived.GetPrivate("PublicStatic"));
-            Assert.IsNotNull(derived.GetPrivate("PublicVirtual"));
+            public Base()
+            {
+                Protected = "protected member";
+                ProtectedStatic = "protected static member";
+                ProtectedVirtual = "protected virtual member";
+                Public = "public member";
+                PublicStatic = "public static member";
+                PublicVirtual = "public virtual member";
+            }
+        }
+
+        private class Derived : Base
+        {
         }
 
         [TestMethod]
-        public void CanSetMembers()
+        public void CanGetAndSetMembers()
         {
             var derived = new Derived();
 
@@ -69,6 +54,15 @@ namespace Sample
             Assert.AreEqual("changed", derived.GetPrivate("Public"));
             Assert.AreEqual("changed", derived.GetPrivate("PublicStatic"));
             Assert.AreEqual("changed", derived.GetPrivate("PublicVirtual"));
+        }
+
+        [TestMethod]
+        public void CanSpecifyReturnValueType()
+        {
+            var derived = new Derived();
+
+            string value = derived.GetPrivate<string>("_private");
+            Assert.IsNotNull(value);
         }
     }
 }
---
+++ Sample/UnitTest2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sample
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private class Base
+        {
+            private string _private = "base";
+
+            private string Property { get { return "property"; } }
+        }
+
+        private class Derived : Base
+        {
+            private string _private = "derived";
+
+            public string Property()
+            {
+                return "method";
+            }
+        }
+
+        [TestMethod]
+        public void RealTypeIsPriority()
+        {
+            var derivedAsBase = new Derived() as Base;
+
+            Assert.AreEqual("derived", derivedAsBase.GetPrivate("_private"));
+        }
+
+        [TestMethod]
+        public void MethodIsIgnored()
+        {
+            var derived = new Derived();
+
+            Assert.AreEqual("property", derived.GetPrivate("Property"));
+        }
+    }
+}
