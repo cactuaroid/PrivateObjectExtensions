480eee2a7f79a5bb4b5aefb07fee2821ae213a2d
cactuaroid <solamiredoredore@gmail.com>
2016-10-09 13:54:45 -0700

    Remove useless using.

--- Sample/Usage1.cs
+++ Sample/Usage1.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
 
